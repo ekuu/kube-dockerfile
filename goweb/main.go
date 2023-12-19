4143531cbf4b0adef88d2a8980804a0632575326
ekuu <ekuu@foxmail.com>
2023-12-19 15:59:47 +0800

    add env

--- goweb/main.go
+++ goweb/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world."))
+		w.Write([]byte(fmt.Sprintf("hello world, my name: %s\n", os.Getenv("MY_NAME"))))
 	})
 	log.Println("listening...")
 	if err := http.ListenAndServe(":80", nil); err != nil {
